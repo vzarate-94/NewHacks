e32beaac3a5be30e0b5b0c77d899229b9a12f63d
vzarate26 <vzarate26@gmail.com>
2022-03-14 15:45:51 -0400

    change heading of landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,7 @@ const Landing = ({user}) => {
   return (
     <main className={styles.container}>
       <h1>
-        hello, {user ? user.name : "friend"}
+        Todays Top Hacks
       </h1>
     </main>
   )
