23206b2f0e84e6f6b0c65920556396da68ea5b16
vzarate26 <vzarate26@gmail.com>
2022-03-21 22:33:47 -0400

    add future profile feed to profile component

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,12 +1,14 @@
 import styles from './Landing.module.css'
 import Profile from '../../components/Profile/Profile'
 import DarkMode from '../../components/DarkMode/DarkMode'
+import Feed from '../../components/Feed/Feed'
 
 const Landing = ({user}) => {
   return (
     <main className={styles.container}>
       <Profile />
       <DarkMode />
+      <Feed />
       <h1>
         Todays Top Hacks
       </h1>
