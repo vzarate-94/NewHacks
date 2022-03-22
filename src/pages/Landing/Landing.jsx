2c751d6a4d786a05904955bbb4c8b3be00fe1f12
vzarate26 <vzarate26@gmail.com>
2022-03-21 22:32:47 -0400

    add future dark mode component to profile page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import styles from './Landing.module.css'
 import Profile from '../../components/Profile/Profile'
+import DarkMode from '../../components/DarkMode/DarkMode'
 
 const Landing = ({user}) => {
   return (
     <main className={styles.container}>
       <Profile />
+      <DarkMode />
       <h1>
         Todays Top Hacks
       </h1>
