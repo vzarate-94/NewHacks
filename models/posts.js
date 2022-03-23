670c4fd5623283303f050cc165c0b66139872c87
vzarate26 <vzarate26@gmail.com>
2022-03-22 22:30:20 -0400

    create comment schema and added to post schema

--- models/posts.js
+++ models/posts.js
@@ -2,12 +2,28 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  comment_text: {
+    type: String,
+    Required: true
+  },
+  commenter: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, { timestamps: true })
+
 const postSchema = new Schema({
-  hackPost: {
+  title: {
     type: String, 
     required: true
-  }
-})
+  },
+  postUrl: {
+    type: String,
+  },
+  added_by: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User' },
+  comments: [commentSchema],
+  }, { timestamps: true })
+
 
 const Post = mongoose.model('Post', postSchema)
 
