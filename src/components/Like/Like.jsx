e02617142dff8a380f3c614d28236a33d62860e0
vzarate26 <vzarate26@gmail.com>
2022-04-13 23:26:26 -0400

    add count functionality to like button

--- src/components/Like/Like.jsx
+++ src/components/Like/Like.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
+import '../../styles/Card.css'
 
 function Like() {
+  const [likeCount, setLikeCount] = useState(1)
+
+  const handleLikeCount = () => {
+    setLikeCount(likeCount + 1)
+  }
   return (
-    <div>
-      <button>Like</button>
-      </div>
+    <div className='like-div'>
+      <button id='like-button' onClick={handleLikeCount}>Like</button>
+      <p id='like-count' ><strong>{likeCount}</strong></p>
+    </div>
   )
 }
 
