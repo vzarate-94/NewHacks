1b6e01da9a93b1c591052e3dab117fe16ba269d6
vzarate26 <vzarate26@gmail.com>
2022-04-16 11:11:22 -0400

    fix view comment component styling

--- src/components/Comment/CommentButton.jsx
+++ src/components/Comment/CommentButton.jsx
@@ -4,9 +4,9 @@ import '../../styles/Card.css'
 
 function CommentButton(props) {
   return (
-    <button id='comment-button'>
-      <Link to={`/post/${props.post._id}`}>View Comments</Link>
-    </button>
+    <Link to={`/post/${props.post._id}`}>
+      View Comments
+    </Link>
   )
 }
 
