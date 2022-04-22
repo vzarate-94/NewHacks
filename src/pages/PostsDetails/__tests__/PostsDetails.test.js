edb2a4899e6e50e979e854244d6bd1dc75d2e162
vzarate26 <vzarate26@gmail.com>
2022-04-22 13:45:09 -0400

    add more unit test

--- src/pages/PostsDetails/__tests__/PostsDetails.test.js
+++ src/pages/PostsDetails/__tests__/PostsDetails.test.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PostDetails from '../PostDetails';
 import UserCard from '../../../components/misc/UserCard/UserCard';
 import PostDetailsHeader from '../PostDetailsHeader';
 
-import { render, cleanup } from '@testing-library/react';
+
+import {  cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import renderer from 'react-test-renderer';
 
 afterEach(cleanup);
 
+
 it('UserCard Component render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<UserCard></UserCard>, div);
 });
+// Import that the UserCard component renders correctly.
 
 it('PostDetailsHeader renders without crashing', () => {
   const div = document.createElement('div');
@@ -25,3 +27,6 @@ it('matches Snapshot', () => {
   const tree = renderer.create(<PostDetailsHeader />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+
+
