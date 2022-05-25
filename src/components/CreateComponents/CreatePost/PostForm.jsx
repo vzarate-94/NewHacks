3fdd0b2d39fbe309d2970018c4f74ca973730001
vzarate26 <vzarate26@gmail.com>
2022-05-25 10:35:56 -0400

    add other option to post categories

--- src/components/CreateComponents/CreatePost/PostForm.jsx
+++ src/components/CreateComponents/CreatePost/PostForm.jsx
@@ -29,13 +29,14 @@ const PostForm = (props) => {
                     onChange={e => props.setHackDescription(e.target.value)} />
 
                 <select value={props.postCategory} onChange={e => props.setPostCategory(e.target.value)}>
-                    <option value="finacne">Finance</option>
-                    <option value="cleaning">Cleaning</option>
+                    <option value="Finacne">Finance</option>
+                    <option value="Cleaning">Cleaning</option>
                     <option value="DIY">DIY</option>
                     <option value="Personal Hygene">Personal Hygene</option>
                     <option value="Mental Health">Mental Health</option>
                     <option value="Tech">Tech</option>
                     <option value="Travel">Travel</option>
+                    <option value="Other">Other</option>
                 </select>
                 <div className='url-inputs'>
                 <input 
