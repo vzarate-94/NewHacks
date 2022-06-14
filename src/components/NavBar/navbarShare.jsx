4eb83606715f81cff8390db33a0ca7bb5cfcdc8b
vzarate26 <vzarate26@gmail.com>
2022-06-13 23:57:00 -0400

    create and flesh out navbarShare component

---
+++ src/components/NavBar/navbarShare.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class navbarShare extends Component {
+  render() {
+    return (
+      <div>navbarShare</div>
+    )
+  }
+}
+
+export default navbarShare
