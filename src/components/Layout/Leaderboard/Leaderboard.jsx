de2756b2994931c709a7d87b99e326ee988a01c0
vzarate26 <vzarate26@gmail.com>
2022-10-24 00:50:05 -0400

    create Links with react-Router-Dom to each categorie with their names

--- src/components/Layout/Leaderboard/Leaderboard.jsx
+++ src/components/Layout/Leaderboard/Leaderboard.jsx
@@ -12,16 +12,15 @@ const Leaderboard = (props) => {
                     <h3>Categories</h3>
                 </div>
                 <div className="leader-card" >
-                    <Link className='redirect-link' to="/finance">
-                    <h5>Finance</h5></Link>
-                    <h5>Cleaning</h5>
-                    <h5>DIY</h5>
-                    <h5>Personal Health</h5>
-                    <h5>Mental Health</h5>
-                    <h5>Tech</h5>
-                    <h5>Travel</h5>
-                    <h5>Funny</h5>
-                    <h5>Other</h5>
+                    <Link to="/finance"><h5>Finance</h5></Link>
+                    <Link to="/finance"><h5>Cleaning</h5></Link>
+                    <Link to="/DIY"><h5>DIY</h5></Link>
+                    <Link to="/PersonalHealth"><h5>Personal Health</h5></Link>
+                    <Link to="/MentalHealth"><h5>Mental Health</h5></Link>
+                    <Link to="/Tech"><h5>Tech</h5></Link>
+                    <Link to="/Travel"><h5>Travel</h5></Link>
+                    <Link to="/Funny"><h5>Funny</h5></Link>
+                    <Link to="/Other"><h5>Other</h5></Link>
                 </div>
                 
             </div>
