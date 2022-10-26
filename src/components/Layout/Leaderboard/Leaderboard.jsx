1315bf4e2a99de19b292b38f8950e923c32e660e
vzarate26 <vzarate26@gmail.com>
2022-10-25 21:36:51 -0400

    add css to links in leaderboard

--- src/components/Layout/Leaderboard/Leaderboard.jsx
+++ src/components/Layout/Leaderboard/Leaderboard.jsx
@@ -12,16 +12,16 @@ const Leaderboard = (props) => {
                     <h3>Categories</h3>
                 </div>
                 <div className="leader-card" >
-                    <Link className='category-link' to="/finance">
+                    <Link id='category-link' to="/finance">
                     <h5>Finance</h5></Link>
-                    <Link className='category-link' to="/finance"><h5>Cleaning</h5></Link>
-                    <Link className='category-link' to="/DIY"><h5>DIY</h5></Link>
-                    <Link className='category-link' to="/PersonalHealth"><h5>Personal Health</h5></Link>
-                    <Link className='category-link' to="/MentalHealth"><h5>Mental Health</h5></Link>
-                    <Link className='category-link' to="/Tech"><h5>Tech</h5></Link>
-                    <Link className='category-link' to="/Travel"><h5>Travel</h5></Link>
-                    <Link className='category-link' to="/Funny"><h5>Funny</h5></Link>
-                    <Link className='category-link' to="/Other"><h5>Other</h5></Link>
+                    <Link id='category-link' to="/finance"><h5>Cleaning</h5></Link>
+                    <Link id='category-link' to="/DIY"><h5>DIY</h5></Link>
+                    <Link id='category-link' to="/PersonalHealth"><h5>Personal Health</h5></Link>
+                    <Link id='category-link' to="/MentalHealth"><h5>Mental Health</h5></Link>
+                    <Link id='category-link' to="/Tech"><h5>Tech</h5></Link>
+                    <Link id='category-link' to="/Travel"><h5>Travel</h5></Link>
+                    <Link id='category-link' to="/Funny"><h5>Funny</h5></Link>
+                    <Link id='category-link' to="/Other"><h5>Other</h5></Link>
                 </div>
                 
             </div>
